5e707663f3f0f595f8dd8335a2b187f744cad680
ozyx <jessemazzella@gmail.com>
2018-10-04 17:18:26 -0700

    Fix side nav not closing properly on mobile

--- src/components/navigation_side/index.js
+++ src/components/navigation_side/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import Link from "react-router-dom/es/Link";
 
 export default ({name}) => {
     return <div id='offcanvas-navbar' uk-offcanvas="mode: reveal; flip: true;">
         <div className='uk-offcanvas-bar'>
             <ul className="uk-nav uk-nav-default">
-                <li className={name === "about" ? "uk-active" : ""}><Link to='/about'>About Us</Link></li>
-                <li className={name === "board" ? "uk-active" : ""}><Link to='/board'>The Board</Link></li>
+                <li className={name === "about" ? "uk-active" : ""}><a href='/about'>About Us</a></li>
+                <li className={name === "board" ? "uk-active" : ""}><a href='/board'>The Board</a></li>
                 <li className={name === "events" ? "uk-active uk-parent" : "uk-parent"}><a href='/events'>Events</a>
                         <ul className='uk-nav-sub'>
                             <li className={name === "events" ? "uk-active" : ""}><a href='/events'>Schedule</a></li>
-                            <li className={name === "seminars" ? "uk-active" : ""}><Link to='/seminars'>Seminars</Link></li>
+                            <li className={name === "seminars" ? "uk-active" : ""}><a href='/seminars'>Seminars</a></li>
                         </ul>
                 </li>
-                <li className={name === "contact" ? "uk-active" : ""}><Link to='/contact'>Contact Us</Link></li>
-                <li className={name === "subscribe" ? "uk-active" : ""}><Link to='/subscribe'>Get Involved</Link></li>
+                <li className={name === "contact" ? "uk-active" : ""}><a href='/contact'>Contact Us</a></li>
+                <li className={name === "subscribe" ? "uk-active" : ""}><a href='/subscribe'>Get Involved</a></li>
             </ul>
         </div>
     </div>

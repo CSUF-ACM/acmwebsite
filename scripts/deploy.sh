f85d5a2ed031cc5932259787e470c2540954a07e
ozyx <jessemazzella@gmail.com>
2019-01-25 12:41:22 -0800

    try old method of deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Deploy changes to server
-sshpass -p $SERVER_PASS scp -o 'StrictHostKeyChecking no' -r build/* $SERVER_USER@$SERVER_URL:~/homepage
+sshpass -p $SERVER_PASS scp -r build/* $SERVER_USER@$SERVER_URL:~/homepage

0906e3b2c8efff8844fb728b8cc89d34b73abeb0
ozyx <jessemazzella@gmail.com>
2019-01-26 14:09:35 -0800

    add verbose option to deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Deploy changes to server
-sshpass -p $SERVER_PASS scp -r build/* $SERVER_USER@$SERVER_URL:~/homepage
+sshpass -p $SERVER_PASS scp -v -r build/* $SERVER_USER@$SERVER_URL:~/homepage

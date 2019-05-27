e9a4f28cf900f34a8e695325c2f40d00ac92a265
Mass Kizar Cassiere <kcassiere97@csu.fullerton.edu>
2019-05-26 17:58:11 -0700

    Fall 2019 Board update

--- src/containers/board/index.js
+++ src/containers/board/index.js
@@ -9,6 +9,7 @@ import BoardImage from '../../media/blank.jpg';
 import Fall2018List from '../../lists/Fall2018List';
 import Spring2018List from '../../lists/Spring2018List';
 import Spring2019List from '../../lists/Spring2019List';
+import Fall2019List from '../../lists/Fall2019List';
 import AdvisorsList from "../../lists/AdvisorsList";
 
 const Board = () => {
@@ -21,6 +22,7 @@ const Board = () => {
             <ul className="uk-flex-center uk-flex uk-subnav uk-subnav-pill"
                 uk-switcher="animation: uk-animation-scale-up; swiping:true;">
                 {/* Add the button to see more board members here! */}
+                <li><a href='#fall2019-members'>Fall 2019</a></li>
                 <li><a href='#spring2019-members'>Spring 2019</a></li>
                 <li><a href='#fall2018-members'>Fall 2018</a></li>
                 <li><a href='#spring2018-members'>Spring 2018</a></li>
@@ -28,6 +30,7 @@ const Board = () => {
             </ul>
             <ul className="uk-margin-small-right uk-margin-small-left uk-switcher">
                 {/* Add new board members here follow the same format! */}
+                <li id='fall2019-members'><CardList members={Fall2019List}/></li>
                 <li id='spring2019-members'><CardList members={Spring2019List}/></li>
                 <li id='fall2018-members'><CardList members={Fall2018List}/></li>
                 <li id='spring2018-members'><CardList members={Spring2018List}/></li>
---
+++ src/lists/Fall2019List.js
@@ -0,0 +1,127 @@
+import JamesImage from '../media/james_sml.jpg';
+import KennethImage from '../media/Kenneth.png';
+import JacobImage from '../media/jacob_sml.jpg';
+import MadiImage from '../media/Madi2.png';
+import ShivamImage from '../media/Shivam.png';
+import HollandImage from '../media/Holland.jpg';
+import JasonImage from '../media/Jason_sml.png';
+import RadhikaImage from '../media/Radhika.jpg';
+import GracieImage from '../media/Graciela.jpg';
+import TaylorImage from '../media/Taylor_sml.png';
+import KizarImage from '../media/Kizar.jpg';
+
+export default [
+    {
+        id: 1,
+        image: JamesImage,
+        name: "James Werrett",
+        title: "President",
+        description: "Manages established programs and resources. Responsible for the creation of future programs.",
+        email: "jswerrett@gmail.com",
+        linkedin: "https://www.linkedin.com/in/james-werrett/",
+        github: "https://github.com/jswerrett"
+    },
+
+    {
+        id: 2,
+        image: KennethImage,
+        name: "Kenneth Aguilar",
+        title: "Vice President",
+        description: "Primary Support on Deliverables.",
+        email: "kennethaguilar.cs@gmail.com",
+        linkedin: "https://www.linkedin.com/in/kenneth-aguilar/",
+        github: "https://github.com/K-ennethA"
+    },
+
+    {
+        id: 3,
+        image: JasonImage,
+        name: "Jason Jose",
+        title: "Treasurer",
+        description: "Handles all financial and budgeting affairs. Maintains Associated Students Inc. account.",
+        email: "jasonsisonjose@csu.fullerton.edu",
+        linkedin: "https://www.linkedin.com/in/jason-jose-a6033914a/"
+    },
+
+    {
+        id: 4,
+        image: GracieImage,
+        name: "Graciela Orozco",
+        title: "Secretary",
+        description: "Meeting minutes, send out emails, communication",
+        email: "orozco.gracie.m@gmail.com",
+        linkedin: "https://www.linkedin.com/in/graciela-orozco/",
+        github: "https://github.com/gracieorozco"
+    },
+
+    {
+        id: 5,
+        image: RadhikaImage,
+        name: "Radhika Sharma",
+        title: "Marketing Coordinator",
+        description: "Publicizes events and programs. Responsible for Social Media accounts.",
+        email: "radhiwells@gmail.com"
+    },
+
+    {
+        id: 6,
+        image: JacobImage,
+        name: "Jacob Powell",
+        title: "Project Manager",
+        description: "Creates new programs and manages current programs. Develops student projects and workshops.",
+        email: "scoutdude@csu.fullerton.edu",
+        linkedin: "https://www.linkedin.com/in/jacob-powell-925571121/",
+        github: "https://github.com/Scout2012"
+    },
+
+    {
+        id: 7,
+        image: MadiImage,
+        name: "Madison Jordan",
+        title: "Project Manager",
+        description: "Creates new programs and manages current programs. Develops student projects and workshops.",
+        email: "madisonjordan@csu.fullerton.edu",
+        linkedin: "https://www.linkedin.com/in/madison-jordan-6b2997182/"
+    },
+
+      {
+        id: 8,
+        image: ShivamImage,
+        name: "Shivam Shah",
+        title: "Workshop Manager",
+        description: "Plans, produces, and executes workshops.",
+        email: "shivamshah98@csu.fullerton.edu",
+        linkedin: "https://www.linkedin.com/in/shivam-shah-296576182/"
+    },
+
+    {
+        id: 9,
+        image: HollandImage,
+        name: "Holland Ho",
+        title: "Competition Manager",
+        description: "Organizes and leads teams and practices for competitions.",
+        email: "ho.holland1999@csu.fullerton.edu",
+        linkedin: "https://www.linkedin.com/in/holland-ho-285873172",
+        github: "https://github.com/HollyBakez"
+    },
+    {
+        id: 10,
+        image: TaylorImage,
+        name: "Taylor Noh",
+        title: "Historian",
+        description: "Manages the ACM news site and writes about recent ACM events.",
+        email: "taylornoh@csu.fullerton.edu",
+        linkedin: "https://www.linkedin.com/in/taylor-noh-06356817a/"
+    },
+    {
+        id: 11,
+        image: KizarImage,
+        name: "Kizar Cassiere",
+        title: "Web Master",
+        description: "Maintains the official website and manages ACM GitHub repositories. Benevolent web overlord.",
+        email: "kcassiere97@gmail.com",
+        github: "https://github.com/kcassiere97",
+        linkedin: "https://www.linkedin.com/in/kcassiere97/"
+    }
+
+];

d7e1807c87e3b7ad943e2379556b39e34fb847d4
Mass Kizar Cassiere <kcassiere97@csu.fullerton.edu>
2019-09-10 18:26:16 -0700

    mailchimp

--- src/containers/subscribe/index.js
+++ src/containers/subscribe/index.js
@@ -16,8 +16,11 @@ const Subscribe = () => {
             <div className='uk-container'>
                 {/* If you want to change the list look for the hidden input value and the form action field */}
                 <MailingList
-                    list_url={'https://fullerton.us11.list-manage.com/subscribe/post?u=3dbde654283a22318e4cd016d&id=693fc5f30b'}
-                    hidden_val={'b_3dbde654283a22318e4cd016d_693fc5f30b'}
+                    // list_url={'https://fullerton.us11.list-manage.com/subscribe/post?u=3dbde654283a22318e4cd016d&id=693fc5f30b'}
+                    // hidden_val={'b_3dbde654283a22318e4cd016d_693fc5f30b'}
+
+                    list_url={'https://fullerton.us20.list-manage.com/subscribe/post?u=5f4a2c9b86143b9f996fd5586&amp;id=747b0f1f03'}
+                    hidden_val={'b_5f4a2c9b86143b9f996fd5586_747b0f1f03'}
                 />
             </div>
         </div>
